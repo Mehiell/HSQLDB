79249d51565cef8ef69d835be4a2612c80fccdba
Peter Shen <zpshen@gmail.com>
2011-04-18 10:36:57 +0800

    setup env

--- build/setenv.cmd
+++ build/setenv.cmd
@@ -1,7 +1,7 @@
 REM basic environment setup
 REM change as needed
 
-set JAVA_HOME=C:\Program Files\Java\jdk7
-set ANT_HOME=C:\java\lib\ant
+set JAVA_HOME=D:\jdk1.6.0
+set ANT_HOME=D:\apache-ant-1.8.1
 set ANT_OPTS=-Xmx512m
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
